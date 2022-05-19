d192ada04911664791352eb469a5726800d979fd
rokas <rokasra@gmail.com>
2022-05-19 10:46:45 +0300

    checkRequired, checkMinLength

--- public/js/modules/validation.js
+++ public/js/modules/validation.js
@@ -11,34 +11,44 @@ function addError(message, field) {
   });
 }
 
+function checkRequired(value, field) {
+  if (value === '') {
+    addError('this field is required', field);
+    return true;
+  }
+  return false;
+}
+
+function checkMinLength(value, minLength, field) {
+  if (value.length <= minLength) {
+    addError(`length must be greater than ${minLength}`, field);
+  }
+}
+
 // rules ['required', 'minLength-4']
 export function checkInput(valueToCheck, field, rulesArr) {
   // eslint-disable-next-line no-restricted-syntax
   for (const rule of rulesArr) {
     // rule === required
     if (rule === 'required') {
-      if (valueToCheck === '') {
-        // pranesti apie klaida
-        addError('this field is required', field);
+      if (checkRequired(valueToCheck, field)) {
         return;
       }
     }
     // rule === minLength-X
     if (rule.split('-')[0] === 'minLength') {
       const min = rule.split('-')[1];
-      if (valueToCheck.length <= min) {
-        addError(`min length must be greater than ${min}`, field);
-      }
+      checkMinLength(valueToCheck, min, field);
     }
     // rule === maxLength-X
     if (rule.split('-')[0] === 'maxLength') {
       const max = rule.split('-')[1];
       if (valueToCheck.length >= max) {
-        addError(`Pass is too long. Length must be less than ${max}`, field);
+        addError(`Too long. Length must be less than ${max}`, field);
       }
     }
 
-    // rule === email tikrinam ar yra @ raide
+    // rule ===  email tikrinam ar yra @ raide
     // extra ar yra '.' po @ raide
   }
 }
